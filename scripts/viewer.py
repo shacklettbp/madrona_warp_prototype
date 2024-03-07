c4002e93310e05cf442e9a491569539a2284f030
Brennan Shacklett <bpshacklett@gmail.com>
2024-03-07 13:45:34 -0800

    Add headless scripts

--- scripts/viewer.py
+++ scripts/viewer.py
@@ -1,13 +1,8 @@
 import torch
-from madrona_warp_proto_sim import SimManager, madrona
 from madrona_warp_proto_viz import VisualizerGPUState, Visualizer
+from sim import Simulator
 
-import numpy as np
 import argparse
-import math
-from pathlib import Path
-import warnings
-warnings.filterwarnings("error")
 
 torch.manual_seed(0)
 
@@ -17,31 +12,14 @@ arg_parser.add_argument('--num-worlds', type=int, required=True)
 arg_parser.add_argument('--window-width', type=int, required=True)
 arg_parser.add_argument('--window-height', type=int, required=True)
 
-arg_parser.add_argument('--gpu-sim', action='store_true')
+arg_parser.add_argument('--cpu-sim', action='store_true')
 
 args = arg_parser.parse_args()
 
 viz_gpu_state = VisualizerGPUState(args.window_width, args.window_height, args.gpu_id)
 
-sim = SimManager(
-    exec_mode = madrona.ExecMode.CUDA if args.gpu_sim else madrona.ExecMode.CPU,
-    gpu_id = args.gpu_id,
-    num_worlds = args.num_worlds,
-    max_episode_length = 500,
-    enable_batch_renderer = True,
-    batch_render_view_width = 64,
-    batch_render_view_height = 64,
-    visualizer_gpu_handles = viz_gpu_state.get_gpu_handles(),
-)
-sim.init()
+sim = Simulator(args.gpu_id, args.num_worlds, args.cpu_sim,
+                viz_gpu_state.get_gpu_handles())
 
-visualizer = Visualizer(viz_gpu_state, sim)
-
-def step_fn():
-    sim.process_actions()
-
-    # Warp here
-
-    sim.post_physics()
-
-visualizer.loop(sim, step_fn)
+visualizer = Visualizer(viz_gpu_state, sim.madrona)
+visualizer.loop(sim.madrona, lambda: sim.step())
