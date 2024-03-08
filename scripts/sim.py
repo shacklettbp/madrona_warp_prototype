ac4c146fc372d2cdcad7afc2439c9fc09bd54ce6
Erwin Coumans <ecoumans@nvidia.com>
2024-03-07 17:47:20 -0800

    add initial Warp env files, no sync yet

--- scripts/sim.py
+++ scripts/sim.py
@@ -1,9 +1,13 @@
 from madrona_warp_proto_sim import SimManager, madrona
+from warp_envs.env_cartpole import CartpoleEnvironment
 
 class Simulator:
     def __init__(self, gpu_id, num_worlds, cpu_madrona, viz_gpu_hdls=None):
         # Initialize madrona simulator
-
+        self.env_cartpole = CartpoleEnvironment()
+        self.env_cartpole.init()
+        self.env_cartpole.reset()
+        
         self.madrona = SimManager(
             exec_mode = madrona.ExecMode.CPU if cpu_madrona else madrona.ExecMode.CUDA,
             gpu_id = gpu_id,
@@ -15,7 +19,7 @@ class Simulator:
             visualizer_gpu_handles = viz_gpu_hdls,
         )
         self.madrona.init()
-
+        
         self.depth = self.madrona.depth_tensor().to_torch()
         self.rgb = self.madrona.rgb_tensor().to_torch()
 
@@ -23,5 +27,9 @@ class Simulator:
         self.madrona.process_actions()
 
         # Warp here
+        self.env_cartpole.step()
+        
+        #optional
+        self.env_cartpole.render()
 
         self.madrona.post_physics()
