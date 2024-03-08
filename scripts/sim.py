40e9054942439c88144d620654ba06645c9f92b4
Erwin Coumans <ecoumans@nvidia.com>
2024-03-08 11:39:22 -0800

    add wp.kernel compute_transforms todo: DLPack reports an error in sim.py:
    Traceback (most recent call last):
      File "/home/ecoumans/develop/madrona/madrona_warp_prototype/scripts/viewer.py", line 25, in <module>
        visualizer.loop(sim.madrona, lambda: sim.step())
      File "/home/ecoumans/develop/madrona/madrona_warp_prototype/scripts/viewer.py", line 25, in <lambda>
        visualizer.loop(sim.madrona, lambda: sim.step())
      File "/home/ecoumans/develop/madrona/madrona_warp_prototype/scripts/sim.py", line 65, in step
        positions = wp.from_dlpack(self.madrona.rigid_body_positions_tensor())
      File "/home/ecoumans/develop/madrona/warp/warp/dlpack.py", line 442, in from_dlpack
        return _from_dlpack(capsule, dtype=dtype)
      File "/home/ecoumans/develop/madrona/warp/warp/dlpack.py", line 335, in _from_dlpack
        assert PyCapsule_IsValid(capsule, _c_str_dltensor), "Invalid capsule"
    AssertionError: Invalid capsule

--- scripts/sim.py
+++ scripts/sim.py
@@ -1,12 +1,44 @@
 from madrona_warp_proto_sim import SimManager, madrona
 from warp_envs.env_cartpole import CartpoleEnvironment
+from warp_envs.environment import RenderMode
+import warp as wp
+wp.init()
+
+@wp.kernel
+def compute_transforms(
+    shape_body: wp.array(dtype=int),
+    shape_transforms: wp.array(dtype=wp.transform),
+    body_q: wp.array(dtype=wp.transform),
+    # outputs
+    out_positions: wp.array(dtype=wp.vec3),
+    out_rotations: wp.array(dtype=wp.quat),
+):
+    tid = wp.tid()
+    i = shape_body[tid]
+    X_ws = shape_transforms[i]
+    if shape_body:
+        body = shape_body[i]
+        if body >= 0:
+            if body_q:
+                X_ws = body_q[body] * X_ws
+            else:
+                return
+    p = wp.transform_get_translation(X_ws)
+    q = wp.transform_get_rotation(X_ws)
+    out_positions[tid] = p
+    out_rotations[tid] = wp.quat(q[3], q[0], q[1], q[2])
 
 class Simulator:
     def __init__(self, gpu_id, num_worlds, cpu_madrona, viz_gpu_hdls=None):
         # Initialize madrona simulator
+        
+        CartpoleEnvironment.render_mode = RenderMode.NONE
+        CartpoleEnvironment.num_envs = num_worlds
+        
         self.env_cartpole = CartpoleEnvironment()
         self.env_cartpole.init()
         self.env_cartpole.reset()
+        
         
         self.madrona = SimManager(
             exec_mode = madrona.ExecMode.CPU if cpu_madrona else madrona.ExecMode.CUDA,
@@ -29,6 +61,25 @@ class Simulator:
         # Warp here
         self.env_cartpole.step()
         
+        #print("self.madrona.rigid_body_positions_tensor()=",self.madrona.rigid_body_positions_tensor())
+        positions = wp.from_dlpack(self.madrona.rigid_body_positions_tensor())
+        #orientations = wp.from_dlpack(self.madrona.rigid_body_rotations_tensor)
+        
+        #compute_transforms(...)
+        #wp.launch(
+        #    compute_transforms,
+        #    dim=self.env_cartpole.num_envs,
+        #    inputs=[
+        #        self.env_cartpole.model.body_shape,
+        #        self.env_cartpole.model.shape_transform,
+        #        self.env_cartpole.state.body_q,
+        #    ],
+        #    outputs=[
+        #        positions,
+        #        orientations,
+        #    ],
+        #)
+
         #optional
         self.env_cartpole.render()
 
